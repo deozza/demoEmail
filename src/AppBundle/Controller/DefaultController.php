328cd30dcb1ef84d618e07bca3d90b6a7a7bc114
Edenn Touitou <deozza@gmail.com>
2018-11-09 17:19:29 +0100

    dump

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -86,6 +86,8 @@ class DefaultController extends Controller
         $email->setTimestamp($postedEmail['timestamp']);
         $email->setPostRequest(var_export($request->files->all(), true));
 
+        dump($request->files);die;
+
         $this->em->persist($email);
         $this->em->flush();
 
